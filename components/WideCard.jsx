22bf936d9bbbc20ed7fe6a951098013bab12ef7b
christhopermarcelino <christhopermarcelino@gmail.com>
2022-02-18 22:15:32 +0700

    feat: fix `WideCard` margin within Link tag

--- components/WideCard.jsx
+++ components/WideCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function WideCard({ name, src, desc }) {
   return (
-    <div className='flex w-full bg-[#55BBF6]/40 rounded-lg shadow-lg pointer-events-auto ring-1 ring-black ring-opacity-5'>
+    <div className='flex w-full bg-[#55BBF6]/40 rounded-lg shadow-lg my-6 pointer-events-auto ring-1 ring-black ring-opacity-5'>
       <div className='flex-1 w-0 p-4'>
         <div className='flex items-start'>
           <div className='relative w-10 h-10'>
--- pages/peralatan.jsx
+++ pages/peralatan.jsx
@@ -23,50 +23,78 @@ export default function Peralatan() {
                 />
               </a>
             </Link>
-            <WideCard
-              name='Kumospace'
-              src='/images/kumospace.png'
-              desc='Video Meeting Interaktif dan Seru'
-            />
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='Kumospace'
+                  src='/images/kumospace.png'
+                  desc='Video Meeting Interaktif dan Seru'
+                />
+              </a>
+            </Link>
           </div>
           <div className='p-8 space-y-6'>
             <h2 className='text-3xl font-semibold'>Alat Interaktif</h2>
-            <WideCard
-              name='Quizziz'
-              src='/images/quizziz.png'
-              desc='Sajikan Soal Latihan Dengan Gamifikasi'
-            />
-            <WideCard
-              name='Kahoot'
-              src='/images/kahoot.png'
-              desc='Selamat Tinggal Kuis Yang Membosankan'
-            />
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='Quizziz'
+                  src='/images/quizziz.png'
+                  desc='Sajikan Soal Latihan Dengan Gamifikasi'
+                />
+              </a>
+            </Link>
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='Kahoot'
+                  src='/images/kahoot.png'
+                  desc='Selamat Tinggal Kuis Yang Membosankan'
+                />
+              </a>
+            </Link>
           </div>
           <div className='p-8 space-y-6'>
             <h2 className='text-3xl font-semibold'>Tugas dan Ujian</h2>
-            <WideCard
-              name='Google Classroom'
-              src='/images/google_classroom.png'
-              desc='Kebutuhan Pembelajaran di Satu Tempat'
-            />
-            <WideCard
-              name='Classcard'
-              src='/images/classcard.png'
-              desc='Tempat Mengatur Kegiatan Belajar'
-            />
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='Google Classroom'
+                  src='/images/google_classroom.png'
+                  desc='Kebutuhan Pembelajaran di Satu Tempat'
+                />
+              </a>
+            </Link>
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='Classcard'
+                  src='/images/classcard.png'
+                  desc='Tempat Mengatur Kegiatan Belajar'
+                />
+              </a>
+            </Link>
           </div>
           <div className='p-8 space-y-6'>
             <h2 className='text-3xl font-semibold'>Lainnya</h2>
-            <WideCard
-              name='Canva'
-              src='/images/canva.png'
-              desc='Presentasi lebih menarik dengan berbagai alat dan template'
-            />
-            <WideCard
-              name='PaperPass'
-              src='/images/paperpass.png'
-              desc='Cek Plagiasi Tugas Maupun Ujian '
-            />
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='Canva'
+                  src='/images/canva.png'
+                  desc='Presentasi lebih menarik dengan berbagai alat dan template'
+                />
+              </a>
+            </Link>
+            <Link href=''>
+              <a>
+                <WideCard
+                  name='PaperPass'
+                  src='/images/paperpass.png'
+                  desc='Cek Plagiasi Tugas Maupun Ujian '
+                />
+              </a>
+            </Link>
           </div>
         </section>
       </main>
